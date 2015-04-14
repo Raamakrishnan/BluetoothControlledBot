506e2e8d86dbceaf9cb3e5f36a20b43a8731e1a3
nnsriram97 <nnsriram97@users.noreply.github.com>
2015-04-14 12:11:53 +0530

    Changed the input to the pins

--- Arduino/bluetooth/bluetooth.ino
+++ Arduino/bluetooth/bluetooth.ino
@@ -1,43 +1,41 @@
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);         \\Motor pins
   pinMode(3,OUTPUT);
   pinMode(5,OUTPUT);
   pinMode(9,OUTPUT);
   pinMode(11,OUTPUT);
   
-  
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 if(Serial.available())
-{digitalWrite(3,LOW);
- digitalWrite(5,LOW);
-digitalWrite(9,LOW);
-digitalWrite(11,LOW);
+{ digitalWrite(3,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(11,LOW);
   char a=Serial.read();
-  if(a=='B')
+  if(a=='A')
   {
     
-    digitalWrite(3,HIGH);
+    digitalWrite(3,HIGH);     \\ Go Forward 
     digitalWrite(11,HIGH);
   }
-  else if(a=='D')
+  else if(a=='C')
   {
-        digitalWrite(3,HIGH);
-    digitalWrite(11,LOW);
+    digitalWrite(3,HIGH);     \\ Turn Left
+    digitalWrite(9,HIGH);
   }
-  else if(a=='A')
+  else if(a=='B')
   {
-        digitalWrite(5,HIGH);
+    digitalWrite(5,HIGH);     \\ Go Backwards
     digitalWrite(9,HIGH);
 
   }
   else if(a=='C')
   {
-        digitalWrite(5,HIGH);
+    digitalWrite(5,HIGH);     \\ Turn Right
     digitalWrite(11,HIGH);
 
   }
